0ed6b558505ee136f846997f29b4bd8137da4ece
BIRD UP <birdupthemusical@outlook.com>
2020-07-23 22:40:54 +0200

    Remove separate folder with on_ready event

--- events/_setup.py
+++
@@ -1,7 +0,0 @@
-from discord.ext.commands import Bot
-
-from events.on_ready import on_ready
-
-
-def setup(bot: Bot):
-    bot.add_listener(on_ready)
--- launcher.py
+++ launcher.py
@@ -4,6 +4,11 @@ from config import prefix, token
 bot = commands.Bot(command_prefix=prefix)
 
 bot.load_extension('commands._setup')
-bot.load_extension('events._setup')
+
+
+@bot.listen()
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
 
 bot.run(token)
