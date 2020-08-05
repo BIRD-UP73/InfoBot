17b26511ee5b9505d76e19b746f785b9853436f4
BIRD UP <birdupthemusical@outlook.com>
2020-08-05 14:02:09 +0200

    Add doc to bool_to_str

--- util.py
+++ util.py
@@ -1,2 +1,7 @@
 def bool_to_str(b: bool) -> str:
+    """
+    Converts a boolean to 'Yes' or 'No'
+    :param b: the boolean
+    :return: 'Yes' if b is true, 'No' otherwise
+    """
     return 'Yes' if b else 'No'
